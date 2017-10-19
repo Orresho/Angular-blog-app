37dc845ef5c6e41b70394e3b26348df0be368039
unknown <orhan.hajderovic@hotmail.com>
2017-10-19 19:53:02 +0200

    added bootstrap

--- client/src/vendor.ts
+++ client/src/vendor.ts
@@ -5,6 +5,7 @@ import '@angular/core';
 import '@angular/common';
 import '@angular/http';
 import '@angular/router';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import 'rxjs';
 import '@angularclass/hmr';
