696018ce36b4ed12ee792742ff31dee197967f15
unknown <orhan.hajderovic@hotmail.com>
2017-10-22 16:37:42 +0200

    add bcrypjs +  post router

--- server/app.js
+++ server/app.js
@@ -28,8 +28,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middleware
+app.use('/user', users);
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../model/user')
+var User = require('../model/user');
+var bcrypt = require('bcryptjs');
+
+
+router.post('/', function (req, res, next) {
+
+  // Get user information from inputs and store in user object
+  var user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    userName: req.body.userName,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 10)
+  });
+  
+  // Save user object to mongo.
+  user.save(function(err, result){
+    if (err) {
+      return res.status(500).json({
+        title: 'An error occured',
+        error: err
+      });
+    }
+    console.log(events);
+    res.status(200).json({
+      message: 'Success',
+      obj: result
+    });
+  });
+});
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
